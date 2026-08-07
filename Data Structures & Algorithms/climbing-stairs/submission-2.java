382892ccea866af021bb37153f03e104383f473f
Samuel Kapusta <41392930+sam-kapusta@users.noreply.github.com>
2026-08-07 12:31:40 -0700

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int climbStairs(int n) {
+        HashMap<Integer, Integer> visited = new HashMap<>();
+        return dfs(n-1, visited) + dfs(n-2, visited);        
+    }
+
+    private int dfs(int n, HashMap<Integer, Integer> visited){
+        if(visited.containsKey(n)) return visited.get(n);
+        if(n < 0) {
+            return 0;
+        }
+        if(n == 0) {
+            return 1;
+        }
+
+        int count = dfs(n-1, visited) + dfs(n-2, visited);
+        visited.put(n, count);
+        return count;
+    };
+}
